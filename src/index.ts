72d17aa84e9d87f21fb4522d49dd848c552d3da2
Mauricio <mauryvillalba@gmail.com>
2020-09-04 21:20:08 -0300

    typeorm connected to mysql

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,60 @@
+import { createConnection } from 'typeorm';
+import 'reflect-metadata';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
-const app = express();
+const startApp = () => {
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-const PORT = process.env.PORT || '3000';
+  const PORT = process.env.PORT || '3000';
 
-const typeDefs = gql`
+  const typeDefs = gql`
+
   type Query {
     greetings: String
+    users: [User!]
   }
+
+  type User{
+    id: ID,
+    name: String,
+    email: String,
+    hashPassword: String
+  }
+
 `;
 
-const resolvers = {
-  Query: {
-    greetings: (): String => 'Hello!',
-  },
+  const resolvers = {
+    Query: {
+      greetings: (): String => 'Hello!',
+      users: () => {
+        console.log('users');
+      },
+
+    },
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  server.applyMiddleware({ app, path: '/recipes' });
+
+  app.listen(PORT, () => {
+    console.log(`Server running in ${PORT}`);
+    console.log(`Graphql endpoint ${server.graphqlPath}`);
+  });
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: '/recipes' });
+const startDBConnection = async () => {
+  try {
+    await createConnection();
+    // here you can start to work with your entities
+    startApp();
+    console.log('Connected to data base');
+  } catch (error) {
+    console.log('TypeOrm error');
+    console.log(error);
+  }
+};
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${PORT}`);
-  console.log(`Graphql endpoint ${server.graphqlPath}`);
-});
+startDBConnection();
